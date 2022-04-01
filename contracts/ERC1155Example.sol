d9271411f59f5155552069b18937c1dcd6167851
Alexander Bushukin <46033931+Protivsporta@users.noreply.github.com>
2022-04-01 19:55:25 +0300

    Fixed comments

--- contracts/ERC1155Example.sol
+++ contracts/ERC1155Example.sol
@@ -3,31 +3,17 @@
 pragma solidity 0.8.13;
 
 import "./ERC1155URIStorage.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract ERC1155Example is ERC1155URIStorage, AccessControl {
+contract ERC1155Example is ERC1155URIStorage {
     uint256 public constant MIR = 0;
-    uint256 public constant NOWAR = 1;
-
+    
     string tokenURI0 = "ipfs://QmWmtKNRakj45cdfd6CvcuFRccyq5otpKKV7GZiPLrTPoU";
-    string tokenURI1 = "ipfs://QmPw9YvizrWTtNvsrop26ohGsat73zEjKkvXNTmvQDnBas";
-
-    bytes32 public constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
 
     constructor () ERC1155(tokenURI0) {
-        _grantRole(ADMIN, msg.sender);
-        _setURI(MIR, tokenURI0);
-        _setURI(NOWAR, tokenURI1);
+        createCollectible(MIR, 1);
     }
 
     function createCollectible(uint256 _tokenID, uint256 _amount) public {
         _mint(msg.sender, _tokenID, _amount, "");
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
-        return
-        interfaceId == type(IERC1155).interfaceId ||
-        interfaceId == type(IERC1155MetadataURI).interfaceId ||
-        super.supportsInterface(interfaceId);
     }
 }
--- contracts/ERC721Example.sol
+++ contracts/ERC721Example.sol
@@ -3,18 +3,12 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-
-contract ERC721Example is ERC721URIStorage, AccessControl {
+contract ERC721Example is ERC721URIStorage {
 
     uint256 public tokenId;
     string baseTokenURI = "ipfs://QmUtXDnnuyopcC66jeCJgbvXZEsakqXXYsfyLbwdUNbHqw";
-    bytes32 public constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
 
-    constructor () ERC721 ("Valera", "VGCG"){
-        tokenId = 0;
-        _grantRole(ADMIN, msg.sender);
-    }
+    constructor () ERC721 ("Valera", "VGCG") {}
 
     function createCollectible() public {
         _safeMint(msg.sender, tokenId);
@@ -24,12 +18,5 @@ contract ERC721Example is ERC721URIStorage, AccessControl {
 
     function tokenURI(uint256 _tokenId) public view override returns (string memory) {
         return super.tokenURI(_tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, AccessControl) returns (bool) {
-        return
-        interfaceId == type(IERC721).interfaceId ||
-        interfaceId == type(IERC721Metadata).interfaceId ||
-        super.supportsInterface(interfaceId);
     }
 }
